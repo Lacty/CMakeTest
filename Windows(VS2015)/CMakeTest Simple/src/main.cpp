d8978ecbe52e0fc51c7a38c35f7c1458abd788ee
Lacty <akira206@gmail.com>
2016-02-09 12:45:01 +0900

    update

--- Windows(VS2015)/CMakeTest Simple/src/main.cpp
+++ Windows(VS2015)/CMakeTest Simple/src/main.cpp
@@ -1,16 +1,8 @@
 
 #include <iostream>
-#include <unordered_map>
+#include "print/print.hpp"
 
 
 int main() {
-	std::unordered_map<int, int> list = {
-		{ 0, 1 },
-		{ 1, 2 },
-		{ 2, 3 }
-	};
-
-	for (auto it : list) {
-		std::cout << it.second << std::endl;
-	}
+  Print("hoge");
 }
